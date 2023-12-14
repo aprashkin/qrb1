7b2ec3983179a21fb9c0ca968bbb5fb5f7799687
sharova_anastasiya <anastasiysharova1910@gmail.com>
2023-12-14 14:26:28 +0300

    ljgdtou

--- qrb/Views/registration.axaml.cs
+++ qrb/Views/registration.axaml.cs
@@ -1,6 +1,9 @@
-﻿using Avalonia;
+﻿using System.Threading;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 
 namespace qrb.Views;
 
@@ -9,13 +12,59 @@ public partial class registration : Window
     public registration()
     {
         InitializeComponent();
-#if DEBUG
-        this.AttachDevTools();
-#endif
+
     }
 
-    private void InitializeComponent()
+    private void perehod_na_vhod(object? sender, RoutedEventArgs e)
     {
-        AvaloniaXamlLoader.Load(this);
+        new MainWindow().Show();
+        Close();
+    }
+    
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        
+        if (Login1.Text == "")
+        {
+            Login1.Watermark = "Некорректный логин";
+            Login1.Foreground = Brushes.Red;
+        }
+        else
+        {
+            if (parol1.Text != parol2.Text)
+            {
+                parol1.Foreground = Brushes.Red;
+                parol1.Watermark = "Пароли не совпадают";
+                parol2.Foreground = Brushes.Red;
+                parol2.Watermark = "Пароли не совпадают";
+            }
+            else
+            {
+                if (kaptcha1.Text != "BYGX7CZ6")
+                {
+                    kaptcha1.Watermark = "Некорректный ввод капчи";
+                    kaptcha1.Foreground = Brushes.Red;
+                }
+                else
+                {
+                    notificationreg.IsVisible = true;
+                }
+            }
+            
+        }
+
+        
+
+        
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ qrb/Views/regyspeh.axaml.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace qrb.Views;
+
+public partial class regyspeh : Window
+{
+    public regyspeh()
+    {
+        InitializeComponent();
+    }
+
+ 
+}
