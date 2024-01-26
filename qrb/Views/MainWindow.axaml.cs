ccd634a75f0fc4e896ae77692bc5c38380af9309
KseniaV <kseniavolkova555@gmail.com>
2024-01-26 13:45:47 +0300

    workk

--- qrb/Views/MainWindow.axaml.cs
+++ qrb/Views/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
@@ -20,20 +21,78 @@ public partial class MainWindow : Window
         Close();
     }
 
-    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    /*private bool ValidateUserCredentials(string enteredUsername, string enteredPassword)
     {
-        Users users =
-            Registration.Users.FirstOrDefault(u => u.Username == LogText.Text && u.Password == ParolText.Text);
+        // Здесь предполагается, что у вас есть список пользовательских данных
+        Users users = Registrationn;
+
+        // Перебираем элементы списка и сверяем логин и пароль
+        foreach (Users user in users)
+        {
+            if (userCredentials.Username == enteredUsername && userCredentials.Password == enteredPassword)
+            {
+                // Логин и пароль совпадают, пользователь успешно вошел
+                return true;
+            }
+        }
+
+        // Если не найдено совпадение, пользователь не смог войти
+        return false;
+    }*/
+
+    private void Button_OnClick(object sender, RoutedEventArgs e)
+    {
+        string enteredUsername = LogText.Text;
+        string enteredPassword = ParolText.Text;
+
+        /*bool loginSuccessful = ValidateUserCredentials(enteredUsername, enteredPassword);*/
+/**/
+        /*if (loginSuccessful)
+        {
+            // Скрываем текущее окно
+            this.Hide();
+
+            // Здесь можно открыть новое окно (замените MainWindow на ваш класс основного окна)
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Closed += (s, args) => this.Close(); // Закрываем текущее окно при закрытии нового
+            mainWindow.Show();
+        }
+        else
+        {
+            ErrorBorder.IsVisible = true;
+        }*/
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        
+        Users users = Registration.Users.FirstOrDefault(u => u.Username == LogText.Text && u.Password == ParolText.Text);w
 
         if (users is null)
         {
             ErrorBorder.IsVisible = true;
-
         }
         else
         {
             new MainMenu().Show();
             this.Close();
         }
-    }
+    }*/
 }
--- qrb/Views/registration.axaml.cs
+++ qrb/Views/registration.axaml.cs
@@ -6,56 +6,79 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
-
+using Avalonia.OpenGL.Egl;
 using qrb.Models;
+using Npgsql;
 
 namespace qrb.Views;
 
 public partial class Registration : Window
 {
+    private NpgsqlDataSource dataSource;
+    
     public static List<Users> Users = new();
     public Registration()
     {
+        var connectionString = "";
+        dataSource = NpgsqlDataSource.Create(connectionString);
         InitializeComponent();
-
+        vas();
     }
 
+    private void vas()
+    {
+        using (var cmd = dataSource.CreateCommand($"select username from autorise where id = 2"))
+        { 
+            var reader = cmd.ExecuteReader();
+            reader.Read();
+            Login1.Watermark = reader[0].ToString();
+        }
+
     
+    }
     
+    public string log;
+    public string par1;
+    public string par2;
 
     private void Button_OnClick(object? sender, RoutedEventArgs e)
     {
-        /*Login1.Text = String.Empty;
-        parol1.Text = String.Empty;
-        parol2.Text = String.Empty;
-        */
 
-        if (string.IsNullOrEmpty(Login1.Text))
+    
+        Login1.Text = log;
+        parol1.Text = par1;
+        parol2.Text = par2;
+        
+
+        if (string.IsNullOrEmpty(log))
         {
             Login1.Watermark = "Необходимо заполнить";
             Login1.Foreground = Brushes.Red;
         }
 
-        if (string.IsNullOrEmpty(parol1.Text))
+        if (string.IsNullOrEmpty(par1))
         {
             parol1.Watermark = "Необходимо заполнить";
             parol1.Foreground = Brushes.Red;
         }
 
-        if (string.IsNullOrEmpty(parol2.Text))
+        if (string.IsNullOrEmpty(par2))
         {
             parol2.Watermark = "Необходимо заполнить";
             parol2.Foreground = Brushes.Red;
         }
         
-        if (!string.IsNullOrEmpty(Login1.Text) || !string.IsNullOrEmpty(parol2.Text) ||
-            !string.IsNullOrEmpty(parol1.Text))
+        if (!string.IsNullOrEmpty(log) || !string.IsNullOrEmpty(par1) ||
+            !string.IsNullOrEmpty(par2))
         {
-            return;
+            using (var cmd = dataSource.CreateCommand("INSERT INTO autorise (username, password) VALUES ($1, $2)"))
+            {
+                cmd.Parameters.AddWithValue(Login1.Text);
+                cmd.Parameters.AddWithValue(ParolText2.Text);
+                cmd.ExecuteNonQuery();
+            }
         }
-        Users users = new Users(Login1.Text, parol1.Text);
-        Users.Add(users);
-
+        
         MainWindow main = new MainWindow();                          
         main.Show();
         this.Close();
